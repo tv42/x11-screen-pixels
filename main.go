727841bf6ebdb3f2e7463eaf2702a751d0289dd7
Tommi Virtanen <tv@eagain.net>
2014-07-23 14:10:36 -0700

    Prettier error message, usage

--- main.go
+++ main.go
@@ -3,14 +3,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/BurntSushi/xgb"
 	"github.com/BurntSushi/xgb/xproto"
 )
 
+var prog = filepath.Base(os.Args[0])
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", prog)
+	fmt.Fprintf(os.Stderr, "  %s\n", prog)
+	fmt.Fprintf(os.Stderr, "  DISPLAY=:1 %s\n", prog)
+	fmt.Fprintf(os.Stderr, "(the command takes no options)\n")
+}
+
 func main() {
+	log.SetFlags(0)
+	log.SetPrefix(prog + ": ")
+
+	flag.Usage = usage
+	flag.Parse()
+	if flag.NArg() != 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
 	X, err := xgb.NewConn()
 	if err != nil {
 		log.Fatal(err)
